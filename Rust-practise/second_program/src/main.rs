e48fb2af15ab0379fa83ee6c487797775f9e3f15
SaqO(n*logn) <ONLYPROGRAMMING@Sargiss-MacBook-Air.local>
2025-10-26 19:32:31 +0400

    second_program

---
+++ Rust-practise/second_program/src/main.rs
@@ -0,0 +1,77 @@
+#![warn(clippy::all , clippy::pedantic)]
+
+use std::cmp::Ordering;
+
+fn main() {
+    let arr = [-1, 3,5,7,8,10,24,37,42,135];
+
+
+    let result = bin_search(&arr, 135);
+
+    match result {
+        Some((found_value, found_index)) => println!("Found value: {found_value} to {found_index}"),
+        None => println!("Not found")
+    }
+}
+
+fn bin_search(arr: &[i32], desired_value: i32) -> Option<(i32, usize)>{
+    let mut low_bound: usize = 0; 
+    let mut upper_bound = arr.len() - 1; 
+    let mut i: usize = 0;  
+
+    while low_bound <= upper_bound {
+        i += 1 ;
+
+        let mid  = (upper_bound + low_bound) / 2;
+
+        let mid_value = arr[mid];
+
+        // if mid_value == desired_value {
+        //     return  Some((mid_value , mid));
+        // } else if desired_value > mid_value {
+        //     low_bound = mid + 1;
+        // }
+        //  else {
+        //     upper_bound = mid - 1; 
+        //  }
+
+
+        match mid_value.cmp(&desired_value) {
+            Ordering::Equal =>  return Some((mid_value , mid)),
+            Ordering::Greater =>  {
+                upper_bound = match  mid.checked_sub(1) {
+                    Some(result ) => result,
+                    _ => return None,
+                }
+            }
+            Ordering::Less => low_bound = mid + 1,
+        }
+    }
+
+    None 
+} 
+
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+
+    const ARR: [i32; 10] = [-1, 3,5,7,8,10,24,37,42,135];
+
+    #[test] 
+    fn element_found() {
+        assert_eq!((-1, 0) , bin_search(&ARR, -1).unwrap());
+    }
+
+    #[test] 
+    fn element_not_found() {
+        let result = bin_search(&ARR, 1234);
+        assert!(result.is_none());
+    }
+
+     #[test] 
+    fn smallest_element_found() {
+        let result = bin_search(&ARR, -100);
+        assert!(result.is_none());
+    }
+}
